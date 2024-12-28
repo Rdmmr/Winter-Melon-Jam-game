3eea62c72b57858d8d4df183fafc15026456401f
Desmond Jaconi Biery <desmondcjb@gmail.com>
2024-12-27 16:08:42 -0800

    Add files via upload

--- Scenes/reverse_polarity.gd
+++ Scenes/reverse_polarity.gd
@@ -3,6 +3,7 @@ var polarity = "left"
 func _on_body_entered(body):
 	if (body.has_method("setPolar")):
 		body.setPolar("left")
-		var sprite = get_node(body.AnimatedSprite2D)
-		sprite.play("new_animation")
+	if (body.has_node("AnimatedSprite2D")):
+		body.get_node("AnimatedSprite2D").play("new_animation_1");
+
 		print("this works")
