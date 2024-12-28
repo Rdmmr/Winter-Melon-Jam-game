b44b9602bb33fd85108bbe5f938fdae219fcb9c1
Xp-fish <gncxbox1@gmail.com>
2024-12-27 19:19:30 -0800

    Add files via upload

---
+++ winter-melon-jam-game-main/reverse_polarity_r.gd
@@ -0,0 +1,9 @@
+extends Area2D
+var polarity = "left"
+func _on_body_entered(body):
+	if (body.has_method("setPolar")):
+		body.setPolar("right")
+	if (body.has_node("AnimatedSprite2D")):
+		body.get_node("AnimatedSprite2D").play("new_animation");
+
+		print("this works")
